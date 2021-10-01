8fa98264c3b362c5d2e0911c84b013b4082e4c75
Claudio Figueroa <claudio.figueroa@medeiconsultoria.com.br>
2021-10-01 11:28:06 -0300

    Final commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -68,16 +69,61 @@ app.route("/articles/:article_title")
 
 
     .get(function(req, res){
-        Article.findOne({title: req.body.article_title}, function(err, article){
+        const titleSearch = _.trim(req.body.title);
+        const title = _.startCase(titleSearch);
+        Article.findOne({title: title}, function(err, article){
             if(err){
                 res.send(err);
             }
             else{
-                res.send(article);
+                if(article){
+                    res.send(article);
+                }
+                else{
+                    res.send("No article found");
+                }
             }
         });
-    });
+    })
+    
+    .put(function(req, res){
+        const changedArticle = req.params.article_title;
+        console.log(req.body);
+        Article.updateOne({title: changedArticle}, {title: req.body.newTitle, content: req.body.newContent}, function(err){
+            if(err){
+                res.send(err);
+            }
+            else{
+                res.send("Successfully updated");
+            }
+        });
+    })
 
+    .patch(function(req, res){
+        const changedArticle = req.params.article_title;
+        console.log(req.body);
+        Article.updateOne({title: changedArticle}, {$set:{title: req.body.newTitle}}, function(err){
+            if(err){
+                res.send(err);
+            }
+            else{
+                res.send("Successfully updated");
+            }
+        });
+    })
+
+    .delete(function(req, res){
+        Article.deleteOne({title: req.params.article_title}, function(err){
+            if(err){
+                res.send(err);
+            }
+            else{
+                res.send("Successfully deleted");
+            }
+        })
+        
+
+    });
 
 app.listen(3000, function() {
     console.log("Server listening on port 3000");
